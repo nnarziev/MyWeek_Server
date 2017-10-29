405ea0d89971ee81565bc461fbcf2d199d85b73b
Qobiljon <qobiljon.toshnazarov@gmail.com>
2017-10-30 03:10:47 +0900

    Some changes

--- MyWeek_Server/settings.py
+++ MyWeek_Server/settings.py
@@ -29,25 +29,6 @@ ALLOWED_HOSTS = [
     'qobiljon.pythonanywhere.com',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'qobiljon.pythonanywhere.com',
-    'localhost:8000',
-    '127.0.0.1:9000'
-)
-
-CSRF_TRUSTED_ORIGINS = (
-    'qobiljon.pythonanywhere.com',
-)
-
-CORS_ALLOW_METHODS = (
-    'GET',
-    'POST',
-)
-
-CORS_ALLOW_HEADERS = (
-    'content-type',
-)
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -57,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'rest_framework',
     'users',
     'events',
@@ -67,7 +47,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
