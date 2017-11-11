22e5968c9f737d2416b50601eb9fd7e6c8151580
Qobiljon <qobiljon.toshnazarov@gmail.com>
2017-11-11 23:41:34 +0900

    Some changes

--- events/views.py
+++ events/views.py
@@ -80,14 +80,17 @@ def create_event(request):
 
 		if 'event_id' in json_body and Event.objects.filter(event_id=json_body['event_id'], is_active=True).exists():
 			event = Event.objects.get(event_id=json_body['event_id'], is_active=True)
-			event.user = user,
-			event.day = json_body['day'] if 'day' in json_body else event.day,
-			event.start_time = json_body['start_time'] if 'start_time' in json_body else event.start_time,
-			event.length = json_body['length'] if 'length' in json_body else event.length,
-			event.is_active = json_body['is_active'] if 'is_active' in json_body else event.is_active,
-			event.event_name = json_body['event_name'] if 'event_name' in json_body else event.event_name,
-			event.event_note = json_body['event_note'] if 'event_note' in json_body else event.event_note,
+
+			event.user = user
+			event.day = json_body['day'] if 'day' in json_body else event.day
+			event.start_time = json_body['start_time'] if 'start_time' in json_body else event.start_time
+			event.length = json_body['length'] if 'length' in json_body else event.length
+			event.event_name = json_body['event_name'] if 'event_name' in json_body else event.event_name
+			event.event_note = json_body['event_note'] if 'event_note' in json_body else event.event_note
 			event.category_id = json_body['category_id'] if 'category_id' in json_body else event.category_id
+			event.save()
+
+			return Res(data={'result': RES_SUCCESS, 'event_id': event.event_id})
 		else:
 			event = Event.objects.create_event(
 				user=user,
@@ -99,7 +102,7 @@ def create_event(request):
 				event_note='' if 'event_note' not in json_body else json_body['event_note'],
 				category_id=json_body['category_id']
 			)
-		return Res(data={'result': RES_SUCCESS, 'event_id': event.event_id})
+			return Res(data={'result': RES_SUCCESS, 'event_id': event.event_id})
 	else:
 		return Res(data={'result': RES_BAD_REQUEST})
 
